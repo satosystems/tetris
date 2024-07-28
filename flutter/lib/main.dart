1d011096cde7319b4689c4e171cc34dff2f87f3f
Satoshi Ogata <satosystems@gmail.com>
2024-07-28 17:40:23 +0900

    Implement first version tetris

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +12,290 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Tetris',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Tetris(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Tetris extends StatefulWidget {
+  const Tetris({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  TetrisState createState() => TetrisState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TetrisState extends State<Tetris> {
+  static const int boardWidth = 10;
+  static const int boardHeight = 20;
+  static const double cellSize = 30.0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late List<List<bool>> board;
+  late Tetrimino currentTetrimino;
+  late Position position;
+  late Timer timer;
+  int lines = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    startGame();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return KeyboardListener(
+      focusNode: FocusNode()..requestFocus(),
+      onKeyEvent: (KeyEvent event) {
+        if (event is KeyDownEvent || event is KeyRepeatEvent) {
+          switch (event.logicalKey.keyLabel) {
+            case 'Arrow Left':
+              moveLeft();
+              break;
+            case 'Arrow Right':
+              moveRight();
+              break;
+            case 'Arrow Down':
+              moveDown();
+              break;
+            case 'Arrow Up':
+              rotate();
+              break;
+          }
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: RichText(
+            text: TextSpan(
+              children: [
+                const TextSpan(
+                  text: 'Flutter Tetris: ',
+                  style: TextStyle(color: Colors.black),
+                ),
+                TextSpan(
+                  text: 'GitHub',
+                  style: const TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()..onTap = launchURL,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: boardWidth * cellSize,
+                height: boardHeight * cellSize,
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: boardWidth,
+                    childAspectRatio: 1.0,
+                  ),
+                  itemCount: boardWidth * boardHeight,
+                  itemBuilder: (context, index) {
+                    int x = index % boardWidth;
+                    int y = index ~/ boardWidth;
+                    bool isTetriminoCell = currentTetrimino.shape.any((point) =>
+                        point.x + position.x == x && point.y + position.y == y);
+                    bool isFilled = board[y][x] || isTetriminoCell;
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: isFilled ? Colors.grey : Colors.white,
+                        border: Border.all(color: Colors.black, width: 0.1),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Text(
+                'Lines: $lines',
+                style: const TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void startGame() {
+    board = List.generate(
+        boardHeight, (_) => List.generate(boardWidth, (_) => false));
+    currentTetrimino = Tetrimino.random();
+    position = Position(4, 0);
+    lines = 0;
+    timer =
+        Timer.periodic(const Duration(milliseconds: 500), (_) => moveDown());
+  }
+
+  void moveDown() {
+    setState(() {
+      position = Position(position.x, position.y + 1);
+      if (isCollision()) {
+        position = Position(position.x, position.y - 1);
+        fixTetrimino();
+        eraseLines();
+        currentTetrimino = Tetrimino.random();
+        position = Position(4, 0);
+        if (isCollision()) {
+          timer.cancel();
+          startGame();
+        }
+      }
+    });
+  }
+
+  void moveLeft() {
+    setState(() {
+      position = Position(position.x - 1, position.y);
+      if (isCollision()) {
+        position = Position(position.x + 1, position.y);
+      }
+    });
+  }
+
+  void moveRight() {
+    setState(() {
+      position = Position(position.x + 1, position.y);
+      if (isCollision()) {
+        position = Position(position.x - 1, position.y);
+      }
+    });
+  }
+
+  void rotate() {
+    setState(() {
+      currentTetrimino.rotate();
+      if (isCollision()) {
+        currentTetrimino.rotateBack();
+      }
+    });
+  }
+
+  bool isCollision() {
+    for (var point in currentTetrimino.shape) {
+      int x = position.x + point.x;
+      int y = position.y + point.y;
+      if (x < 0 ||
+          x >= boardWidth ||
+          y >= boardHeight ||
+          (y >= 0 && board[y][x])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void fixTetrimino() {
+    for (var point in currentTetrimino.shape) {
+      int x = position.x + point.x;
+      int y = position.y + point.y;
+      if (y >= 0) {
+        board[y][x] = true;
+      }
+    }
+  }
+
+  void eraseLines() {
+    setState(() {
+      int linesBefore = board.length;
+      board.removeWhere((row) => row.every((cell) => cell));
+      int linesAfter = board.length;
+      int linesCleared = linesBefore - linesAfter;
+      lines += linesCleared;
+      for (int i = 0; i < linesCleared; i++) {
+        board.insert(0, List.generate(boardWidth, (index) => false));
+      }
+    });
+  }
+
+  Future<void> launchURL() async {
+    final Uri url =
+        Uri.parse('https://github.com/satosystems/tetris/tree/main/flutter');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw Exception('Could not launch $url');
+    }
+  }
+}
+
+class Position {
+  final int x, y;
+  Position(this.x, this.y);
+}
+
+class Tetrimino {
+  List<Position> shape;
+  Tetrimino(this.shape);
+
+  factory Tetrimino.random() {
+    final shapes = [
+      Tetrimino([
+        Position(0, 0),
+        Position(0, 1),
+        Position(0, 2),
+        Position(0, 3)
+      ]), // I
+      Tetrimino([
+        Position(0, 0),
+        Position(1, 0),
+        Position(0, 1),
+        Position(1, 1)
+      ]), // O
+      Tetrimino([
+        Position(0, 0),
+        Position(-1, 0),
+        Position(1, 0),
+        Position(0, 1)
+      ]), // T
+      Tetrimino([
+        Position(0, 0),
+        Position(-1, 0),
+        Position(0, 1),
+        Position(1, 1)
+      ]), // S
+      Tetrimino([
+        Position(0, 0),
+        Position(1, 0),
+        Position(0, 1),
+        Position(-1, 1)
+      ]), // Z
+      Tetrimino([
+        Position(0, 0),
+        Position(-1, 0),
+        Position(0, 1),
+        Position(0, 2)
+      ]), // J
+      Tetrimino([
+        Position(0, 0),
+        Position(1, 0),
+        Position(0, 1),
+        Position(0, 2)
+      ]), // L
+    ];
+    final random = Random();
+    return shapes[random.nextInt(shapes.length)];
+  }
+
+  void rotate() {
+    shape = shape.map((point) => Position(-point.y, point.x)).toList();
+  }
+
+  void rotateBack() {
+    shape = shape.map((point) => Position(point.y, -point.x)).toList();
   }
 }
